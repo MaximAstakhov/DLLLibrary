062acf0804efb30f9a6a78a2b97f8f79ab010e6b
307 <307@307-7>
2023-07-05 11:13:19 +0300

    Добавьте файлы проекта.

---
+++ DLLLibrary/Hobby.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLLLibrary
+{
+    //Астахов Максим 31 ИС
+    public class Hobby : IComparable<Hobby>//класс хобби использльзование интерфейса ICompare
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Hobby(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+
+        public void Show()
+        {
+            Console.WriteLine($"Хобби: название = {Name}, Описание = {Description}");//вывод на экран
+        }
+        public int CompareTo(Hobby other)
+        {
+            if (this.Description.Length > other.Description.Length) return 1;// название хобби длиннее
+            if (this.Description.Length < other.Description.Length) return -1;//название хобби короче
+            {
+                return Name.CompareTo(other.Name);
+            }
+        }
+    }
+}
---
+++ DLLLibrary/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLLLibrary
+{
+    public class Person
+    {
+        //Астахов Максим 31 ИС
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public List<Hobby> Hobbbies = new List<Hobby>();
+
+        public Person(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+
+        public void ShowAll()
+        {
+            Console.WriteLine($"Человек: имя = {Name}, адрес = {Address}");//вывод на экран
+            Hobbbies.Sort();// сортировка
+            foreach (Hobby hobby in Hobbbies)
+            {
+                hobby.Show();
+            }
+        }
+    }
+}
+
